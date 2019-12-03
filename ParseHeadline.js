e71b787162e4ab385c1ac7aa177eb00c86bfd4ca
bchew01 <54953894+bchew01@users.noreply.github.com>
2019-12-03 11:55:14 -0500

    Update ParseHeadline.js

--- ParseHeadline.js
+++ ParseHeadline.js
@@ -2,7 +2,7 @@ module.exports = {
 
 	ParseHeadline: function(headline) {
 
-		var stopWords = ['i', 'me', 'my', 'myself', 'we', 'us', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
+var stopWords = ['i', 'me', 'my', 'myself', 'we', 'us', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
         'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
         'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'whose', 'this', 'that', 'these',
         'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
@@ -16,7 +16,7 @@ module.exports = {
         'before', 'after', 'above', 'below', 'to', 'from', 'up', 'upon', 'down', 'in', 'out', 'on', 'off', 'over', 'under',
         'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each',
         'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
-        'very', 'say', 'says', 'said', 'shall'];
+        'very', 'say', 'says', 'said', 'shall', ''];
 
 
 		headline = headline.toLowerCase();
